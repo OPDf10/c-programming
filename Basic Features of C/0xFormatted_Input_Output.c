f3acd9c5cdf2b330a6258ff73404498f6d2fa097
Omar <opdfibrahim10@gmail.com>
2026-01-24 22:28:01 +0300

    Studied selection and boolean, and done a task in these 2 concepts

--- Basic Features of C/0xFormatted_Input_Output.c
+++ Basic Features of C/0xFormatted_Input_Output.c
@@ -164,43 +164,199 @@ Write a program that:
 - Avoid "do-nothing" expressions
 */
 
+// #include <stdio.h>
+// #define tax_rate 0.085f
+
+// int main()
+// {
+//     float priceOne, priceTwo, priceThree;
+//     float subtotal = 0.0f;
+//     float tax_amount;
+//     float total;
+//     int counter = 0;
+
+//     printf("%10s", "FILL THE NEXT\n");
+
+//     printf("Item1 Price: $");
+//     scanf("%f", &priceOne);
+//     subtotal += priceOne;
+//     counter++;
+
+//     printf("Item2 Price: $");
+//     scanf("%f", &priceTwo);
+//     subtotal += priceTwo;
+//     counter++;
+
+//     printf("Item3 Price: $");
+//     scanf("%f", &priceThree);
+//     subtotal += priceThree;
+//     counter++;
+//     tax_amount = subtotal * tax_rate;
+//     total = tax_amount + subtotal;
+
+//     printf("\n==== Receipt ====\n");
+//     printf("%-20s%10d\n", "Number of items: ", counter);
+//     printf("%-20s%10.3f\n", "Total before tax: $", subtotal);
+//     printf("%-20s%10.3f\n", "Tax amount: $", tax_amount);
+//     printf("%-20s%10.3f\n", "Total after tax: $", total);
+//     printf("==== The End ====\n");
+
+
+//     return 0; 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*      Intelligent Home Security System
+Handle different security modes (Home, Away, Sleep, Vacation)
+
+Output example:
+
+
+=== HOME SECURITY SYSTEM ===
+
+SENSOR READINGS:
+Door: CLOSED
+Window: OPEN  
+Motion: DETECTED
+Smoke: NONE
+Temperature: 72°F
+Time: 14:30 (Daytime)
+
+SYSTEM STATUS: AWAY mode
+SECURITY LEVEL: HIGH Alert - Intrusion detected!
+ACTIONS: Sound alarm, Notify police, Lights ON
+
+Detailed analysis...
+
+
+*/
+
+
 #include <stdio.h>
-#define tax_rate 0.085f
+#include <stdbool.h>
 
 int main()
 {
-    float priceOne, priceTwo, priceThree;
-    float subtotal = 0.0f;
-    float tax_amount;
-    float total;
-    int counter = 0;
+    int userInput;
 
-    printf("%10s", "FILL THE NEXT\n");
+    //Starting by user mode - Getting data
+    printf("%5s", "===== Welcom to IHSS =====\n");
+    printf("%5s", "USER mode");
+    printf("\nWhich status are you in sir/ma'am ?\n1- Home\n2- Away\n3- Sleep\n4- Vacation\n");
 
-    printf("Item1 Price: $");
-    scanf("%f", &priceOne);
-    subtotal += priceOne;
-    counter++;
+    scanf("%d", &userInput);
 
-    printf("Item2 Price: $");
-    scanf("%f", &priceTwo);
-    subtotal += priceTwo;
-    counter++;
+    switch (userInput)
+    {
+        case 1:
+            printf("Updated! User is in home.");
+            break;
+        case 2:
+            printf("Updated! User is away.");
+            break;
+        case 3:
+            printf("Updated! User is a sleep.");
+            break;
+        case 4:
+            printf("Updated! User is in vacation.");
+            break;
+        default:
+            printf("Your input is wrong, try again!\n");
+            break;
+    }
 
-    printf("Item3 Price: $");
-    scanf("%f", &priceThree);
-    subtotal += priceThree;
-    counter++;
-    tax_amount = subtotal * tax_rate;
-    total = tax_amount + subtotal;
+    // Now, system mode and detection. '0' is open.
+    // Getting data
+    bool door = 1, window = 1, motion = 1, smoke = 1;
+    float temp = 0.0f;
+    int input = 0, hours = 0, mins = 0;
 
-    printf("\n==== Receipt ====\n");
-    printf("%-20s%10d\n", "Number of items: ", counter);
-    printf("%-20s%10.3f\n", "Total before tax: $", subtotal);
-    printf("%-20s%10.3f\n", "Tax amount: $", tax_amount);
-    printf("%-20s%10.3f\n", "Total after tax: $", total);
-    printf("==== The End ====\n");
+    printf("%5s", "\n\nMACHINE mode");
+    printf("%5s", "\nSelect 1 for (Closed) or 0 for (Open):\n");
+    
+    printf("Door is: ");
+    scanf("%d", &input);
+    door = input;
+
+    printf("Window is: ");
+    scanf("%d", &input);
+    window = input;
+
+    printf("Motion is: ");
+    scanf("%d", &input); // 1 for safe, 0 for danger!
+    motion = input;
+
+    printf("Smoke is: ");
+    scanf("%d", &input);  // 1 for safe, 0 for danger!
+    smoke = input;
+
+    printf("%s", "What is the temprature? ");
+    scanf("%f", &temp);
+
+    printf("What is the time now? (Write in form: hours:mins) ");
+    scanf("%d:%d", &hours, &mins);
 
 
-    return 0; 
+    // Now comes the report
+    printf("%5s", "\n\n=== Report Of Your Security Status ===\n");
+    printf("SENSOR READINGS:\n");
+    if (door == 0)
+        printf("Door is: Open!\n");
+    else
+        printf("Door is: closed!\n");
+    
+    if (window == 0)
+        printf("Window is: Open!\n");
+    else
+        printf("Window is: closed!\n");
+
+    if (motion == 0)
+        printf("Motion: DANGER! strange object in move!\n");
+    else
+        printf("Motion: It's all good..\n");
+
+    if (smoke == 0)
+        printf("Smoke: DANGER! HOUSE ON FIRE!\n");
+    else
+        printf("Smoke: It's all good..\n");
+
+    printf("Temperature: %.2f°C\n", temp);
+    printf("Time: %d:%d (Daytime)\n", hours, mins);
+
+    printf("\n1-Home 2-Away 3-Sleep 4-Vacation");
+    printf("\nUSER STATUS: %d\n", userInput);
+    
+    if (motion == 1 && smoke == 1)
+    {
+        if (door == 0 && window == 0)
+        {
+            printf("SECURITY LEVEL: MID Alert - The entrances to the house are not secure!\n");
+        } else if (door == 0 || window == 0) {
+            printf("SECURITY LEVEL: LOW Alert - one of entrances to the house are not secure!\n");
+        } else {
+            printf("Everything is excellent, the house is safe:)\n");
+        }
+    
+    } else {
+        printf("SECURITY LEVEL: HIGH Alert - Intrusion detected!\n");
+    }
+    
+
+
+
+    return 0;
 }
+
+
